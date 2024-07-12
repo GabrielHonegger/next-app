468220ae5c2fdce0168cf8e796c565550d17d203
GabrielHonegger <gabrielhonegger132@gmail.com>
2024-07-12 12:25:34 -0300

    pages to app

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import Link from 'next/link'
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Link href="/sobre">
+                Sobre
+            </Link>
+        </div>
+    )
+}
+
+// This is the component that we want Next.js to render as the Home page.
+export default Home
